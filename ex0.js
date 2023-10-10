3a25bac6079bee2143315d4fc3efc960034fc782
mariaSmartDesign <creativitatidisseny@gmail.com>
2023-10-10 12:13:39 +0200

    ex0

--- ex0.js
+++ ex0.js
@@ -9,6 +9,33 @@
  *
  */
 
-let mes = 4;
+let mes = 5;
 
 // Escribir tu código a partir de aquí
+
+let trimestre;
+
+switch (mes) {
+    case 1:
+    case 2:
+    case 3:
+    console.log("Primer Trimestre");
+    break;
+
+    case 4:
+    case 5:
+    case 6:
+    console.log("Segundo Trimestre");
+    break;
+
+    case 7:
+    case 8:
+    case 9:
+    console.log("Tercer Trimestre");
+    break;
+
+    case 10:
+    case 11:
+    case 12:
+    console.log("Cuarto Trimestre");
+}
