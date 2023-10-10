74ac70f288db80cf71bed6be0aad408391909e17
mariaSmartDesign <creativitatidisseny@gmail.com>
2023-10-10 13:42:36 +0200

    ex9-fet

--- ex9.js
+++ ex9.js
@@ -6,10 +6,11 @@
  * Arreglalo para que la palabra "Ejecutar!" se muestre 10 veces por el terminal
  */
 
-let n=10;
-let i=0;
+let n = 9;
+let i = 0;
 
-while(i<n) {
+while(i<=n) {
     console.log("Ejecutar!")
+    i++
 }
 
